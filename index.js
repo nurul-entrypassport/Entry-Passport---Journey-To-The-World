6594ea4ccff284912aca896b35307d7b94c0f6f0
nurul-entrypassport <nuwule5984@gmail.com>
2025-06-06 15:54:06 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-   export default function Home() {
-     return (
-       <div style={{textAlign: 'center', marginTop: '50px'}}>
-         <h1>Welcome to PDF Viewer App</h1>
-         <a href="/entry-passport" style={{fontSize: '20px', color: '#0070f3'}}>Go to PDF Viewer</a>
-       </div>
-     );
-   }
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div style={{textAlign: 'center', marginTop: '50px'}}>
+      <h1>Welcome to PDF Viewer App</h1>
+      <Link href="/entry-passport">
+        <span style={{
+          fontSize: '20px',
+          color: '#0070f3',
+          textDecoration: 'underline',
+          cursor: 'pointer'
+        }}>
+          Go to PDF Viewer
+        </span>
+      </Link>
+    </div>
+  );
+}
